117d234517eb86e8e83a17de86efde109ae64c6a
Mathew Moon <mmoon@quinovas.com>
2021-08-05 12:19:47 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="cognitoinator",
-    version="0.2.1",
+    version="0.2.2",
     description="Log into Cognito, assume an IAM role, directly access JWT's from your Cognito session, and use boto3 all with Cognito credentials",
     author="Mathew Moon",
     author_email="mmoon@quinovas.com",
